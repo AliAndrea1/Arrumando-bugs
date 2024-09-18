7c40601baae80952d03e2a9c7af9c042f46563fc
Ali Andrea <99283192+AliAndrea1@users.noreply.github.com>
2024-09-18 17:05:34 -0300

    Add files via upload

---
+++ Atividade02/atividade2.js
@@ -0,0 +1,46 @@
+
+function adicionarProduto(id, nome, valor, quantidade) {
+    let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
+    carrinho.push({ id, nome, valor, quantidade });
+    localStorage.setItem('carrinho', JSON.stringify(carrinho));
+    exibirCarrinho(); 
+}
+
+function removerProduto(id) {
+    let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
+    carrinho = carrinho.filter(produto => produto.id != id);
+    localStorage.setItem('carrinho', JSON.stringify(carrinho));
+    exibirCarrinho();
+}
+
+function exibirCarrinho() {
+    let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
+    const listaProdutos = document.getElementById('lista-produtos');
+    listaProdutos.innerHTML = '';
+
+    if (carrinho.length > 0) {
+        carrinho.forEach(produto => {
+            const li = document.createElement('li');
+            li.innerHTML = `
+                ${produto.nome} - Quantidade: ${produto.quantidade} - Valor: R$${produto.valor.toFixed(2)}
+                <button onclick="removerProduto(${produto.id})">Remover</button>
+            `;
+            listaProdutos.appendChild(li);
+        });
+    } else {
+        listaProdutos.innerHTML = 'O carrinho está vazio!';
+    }
+}
+
+// Adicionar manualmente
+function adicionarProdutoManual(event) {
+    event.preventDefault();
+    const id = document.getElementById('produto-id').value;
+    const nome = document.getElementById('produto-nome').value;
+    const valor = parseFloat(document.getElementById('produto-valor').value);
+    const quantidade = parseInt(document.getElementById('produto-quantidade').value);
+
+    adicionarProduto(id, nome, valor, quantidade);
+    document.querySelector('form').reset();
+}
+exibirCarrinho();
